aa3a8a44b9defae0691d6fe99fb4e313cd4ed8f4
山田妃那子 <>
2024-06-12 23:10:42 +0900

    no message

--- app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
+++ app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
@@ -86,7 +86,7 @@ class PostsController extends Controller
     public function commentCreate(Request $request){
 
         $request->validate([
-            'comment' => 'required|string|max:10',
+            'comment' => 'required|string|max:2500',
         ]);
 
         PostComment::create([
